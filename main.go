7dcf26b0fa27de1a338666da213c47d0fe3985dd
kubotake-desktop <keigo1003560@gmail.com>
2025-07-22 21:36:49 +0900

    ginを使ったRESTful API

--- main.go
+++ main.go
@@ -1,34 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-
-type Page struct {
-	Title string
-	Body  []byte
+type album struct {
+	ID	 string  `json:"id"`
+	Title string  `json:"title"`
+	Artist string `json:"artist"`
+	Price float64 `json:"price"`
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
+var albums = []album{
+	{ID: "1", Title: "Album 1", Artist: "Artist 1", Price: 9.99},
+	{ID: "2", Title: "Album 2", Artist: "Artist 2", Price: 14.99},
+	{ID: "3", Title: "Album 3", Artist: "Artist 3", Price: 19.99},
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
+
+func getAlbums(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(context *gin.Context) {
+	var newAlbum album
+
+	if err := context.BindJSON(&newAlbum); err != nil {
+		return
 	}
-	return &Page{Title: title, Body: body}, nil
+	albums = append(albums, newAlbum)
+	context.IndentedJSON(http.StatusCreated, newAlbum)
 }
+
+func getAlbumByID(context *gin.Context) {
+	id := context.Param("id")
+	for _, album := range albums {
+		if album.ID == id {
+			context.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 
 func main() {
-	p1 := &Page{Title: "test", Body: []byte("This is a test page")}
-	p1.save()
-
-	p2, _ := loadPage(p1.Title)
-	fmt.Println(string(p2.Body))
+	r :=  gin.Default()
+	r.GET("/albums", getAlbums)
+	r.POST("/albums", postAlbums)
+	r.GET("/albums/:id", getAlbumByID)
+	r.Run("localhost:8080")
 }
