03d45781c2e4dd8641ff47e33cb551d1c1db4252
kubotake-desktop <keigo1003560@gmail.com>
2025-07-22 22:40:55 +0900

    timeoutを使ったエラーメッセージ処理

--- main.go
+++ main.go
@@ -1,55 +1,37 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/timeout"
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID	 string  `json:"id"`
-	Title string  `json:"title"`
-	Artist string `json:"artist"`
-	Price float64 `json:"price"`
+func testResponse(c *gin.Context) {
+	c.String(http.StatusRequestTimeout, "timeout")
 }
 
-var albums = []album{
-	{ID: "1", Title: "Album 1", Artist: "Artist 1", Price: 9.99},
-	{ID: "2", Title: "Album 2", Artist: "Artist 2", Price: 14.99},
-	{ID: "3", Title: "Album 3", Artist: "Artist 3", Price: 19.99},
+func timeoutMiddleware() gin.HandlerFunc {
+	return timeout.New(
+		timeout.WithTimeout(500*time.Millisecond),
+		// timeout.WithHandler(func(c *gin.Context) {
+		// 	c.Next()
+		// }),
+		timeout.WithResponse(testResponse),
+	)
 }
-
-
-func getAlbums(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, albums)
-}
-
-func postAlbums(context *gin.Context) {
-	var newAlbum album
-
-	if err := context.BindJSON(&newAlbum); err != nil {
-		return
-	}
-	albums = append(albums, newAlbum)
-	context.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-func getAlbumByID(context *gin.Context) {
-	id := context.Param("id")
-	for _, album := range albums {
-		if album.ID == id {
-			context.IndentedJSON(http.StatusOK, album)
-			return
-		}
-	}
-	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
 
 func main() {
-	r :=  gin.Default()
-	r.GET("/albums", getAlbums)
-	r.POST("/albums", postAlbums)
-	r.GET("/albums/:id", getAlbumByID)
-	r.Run("localhost:8080")
+	r := gin.New()
+	r.Use(timeoutMiddleware())
+	r.GET("/slow", func(c *gin.Context) {
+		// 800を200などに変更して試して下さい。
+		time.Sleep(800 * time.Millisecond)
+		c.Status(http.StatusOK)
+	})
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
