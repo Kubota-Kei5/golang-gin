b06911a0f5b582497f02b97eb2b0f564f56d4942
kubotake-desktop <keigo1003560@gmail.com>
2025-07-26 23:42:49 +0900

    oapi-codegen

--- go.mod
+++ go.mod
@@ -11,10 +11,12 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.2.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
+	github.com/getkin/kin-openapi v0.132.0 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/gin-contrib/zap v1.1.5 // indirect
 	github.com/go-openapi/jsonpointer v0.21.1 // indirect
@@ -25,6 +27,9 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/invopop/yaml v0.2.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
@@ -33,7 +38,13 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/oapi-codegen/gin-middleware v1.0.2 // indirect
+	github.com/oapi-codegen/runtime v1.1.2 // indirect
+	github.com/oasdiff/yaml v0.0.0-20250309154309-f31be36b4037 // indirect
+	github.com/oasdiff/yaml3 v0.0.0-20250309153720-d2182401db90 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
 	github.com/swaggo/gin-swagger v1.6.0 // indirect
 	github.com/swaggo/swag v1.16.5 // indirect
--- main.go
+++ main.go
@@ -1,40 +1,120 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
-	"golang-gin/docs"
-
+	// 必要なパッケージはgo getでインストールして下さい。
 	"github.com/gin-gonic/gin"
+	middleware "github.com/oapi-codegen/gin-middleware"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/swag"
+
+	oapi "golang-gin/api"
 )
 
-// HelloWorldExample godoc
-// @Summary hello World
-// @Schemes
-// @Description Hello
-// @Tags Hello World
-// @Accept json
-// @Produce json
-// @Success 200 {string} Helloworld
-// @Router /hello [get]
-func HelloWorld(g *gin.Context) {
-	g.JSON(http.StatusOK, "helloworld")
+type Album struct {
+	// `json:"id,omitempty"`などがコメントアウトされていることに注目して下さい。
+	// この変換は oapi-codgenが生成したstructを使ってMarshalJSONとUnmarshalJSON行っています。
+	ID    int    //`json:"id,omitempty"`
+	Title string //`json:"title"`
 }
 
-func main() {
-	route := gin.Default()
+// デモ用にグローバルでAlbumを作成しています。実際にはデーターベースに格納されます。
+var albums = []Album{
+	{ID: 1, Title: "Test"},
+}
 
-	docs.SwaggerInfo.BasePath = "/api/v1"
-	v1 := route.Group("/api/v1")
-	{
-		eg := v1.Group("/")
-		{
-			eg.GET("/hello", HelloWorld)
+type AlbumHandler struct{}
+
+/*
+GetAlbum関数は、api/api.gen.goの中のインタフェースで定義されているので、GetAlbumという名前にする必要があります。
+openapi.yamlの中のoperationIdで定義されている値が入っています。
+GoのスタイルガイドではAlbumHandlerというstructにAlbumが入っているので、funcではGetAlbumとAlbumを重複して書かないことが推奨されています。
+しかし、oepnapi.yamlの中で重複しない名前を作成し、そこから自動生成しているコードになりますので以下の名前でも問題ありません。
+
+この関数では、アルバムのIDを使用してアルバムを検索し、JSONレスポンスを返します
+*/
+func (a AlbumHandler) GetAlbum(c *gin.Context, ID int) {
+	for _, album := range albums {
+		if album.ID == ID {
+			// OpenAPIに準拠したResponseに変更してresponseを返します。
+			response := oapi.AlbumGetResponse{
+				Id:    &album.ID,
+				Title: album.Title,
+			}
+			c.JSON(http.StatusOK, response)
+			return
 		}
 	}
 
-	route.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-	route.Run(":8080")
+	c.JSON(http.StatusNotFound, nil)
+}
+
+/*
+CreateAlbum関数は、JSONリクエストボディからアルバムを作成し、JSONレスポンスを返します。
+*/
+func (a AlbumHandler) CreateAlbum(c *gin.Context) {
+	var requestBody oapi.AlbumCreateRequest
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	album := Album{
+		Title: requestBody.Title,
+	}
+
+	album.ID = len(albums) + 1
+	albums = append(albums, album)
+	c.JSON(http.StatusCreated, nil)
+}
+
+func main() {
+	router := gin.Default()
+
+	/*
+		以下のコードは、GinサーバーでSwagger UIを表示するためのものであり、oapi.GetSwagger()関数を使用してSwagger YAMLファイルを取得し、
+		json.Marshal()関数を使用してJSON形式に変換します。次に、swag.Spec構造体を使用してSwagger仕様を定義し、swag.Register()関数を
+		使用してSwagger仕様を登録します。最後に、ginSwagger.WrapHandler()関数を使用してSwagger UIを表示するためのGinミドルウェアを
+		設定します。
+
+		SwaggerURL http://0.0.0.0:8080/swagger/index.html
+	*/
+	swagger, _ := oapi.GetSwagger()
+	swaggerJson, _ := json.Marshal(swagger)
+	var SwaggerInfo = &swag.Spec{
+		InfoInstanceName: "swagger",
+		SwaggerTemplate:  string(swaggerJson),
+	}
+	swag.Register(SwaggerInfo.InstanceName(), SwaggerInfo)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	api := router.Group("/api")
+	{
+		/*
+			このミドルウェアは、swagger仕様を使用してリクエストを検証するために使用されます。swagger仕様は、APIの仕様を定義するための
+			オープンスタンダードであり、APIのリクエストとレスポンスの形式を定義します。 middleware.OapiRequestValidator(swagger)は、
+			swagger仕様を引数として受け取り、ginフレームワークのミドルウェアを返します。このミドルウェアは、リクエストを検証し、
+			swagger仕様に従ってリクエストが有効であることを確認します。router.Use()関数を使用して、このミドルウェアをGinサーバーに
+			追加することができます。
+		*/
+		api.Use(middleware.OapiRequestValidator(swagger))
+
+		v1 := api.Group("/v1")
+		{
+			/*
+				`oapi.RegisterHandlers(v1, albumHandler)`は、oapi-codegenによって生成されたGoのAPIコードで使用される
+				関数の1つです。この関数は、APIのエンドポイントとハンドラを登録します。 `v1`パラメータは、Ginルーターのグループを表します。
+				`albumHandler`パラメータは、APIのハンドラを表します。この関数は、APIのエンドポイントとハンドラをマッピングし、
+				APIの実装を完了します。
+			*/
+			albumHandler := AlbumHandler{}
+			oapi.RegisterHandlers(v1, albumHandler)
+
+		}
+	}
+
+	router.Run(":8080")
 }
