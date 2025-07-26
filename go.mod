98336eb002c73fa5a489ac89da577f63a43c8616
kubotake-desktop <keigo1003560@gmail.com>
2025-07-26 12:25:53 +0900

    zapを使ってミドルウェアでロギング

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
+	github.com/gin-contrib/zap v1.1.5 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
@@ -26,6 +27,8 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/arch v0.19.0 // indirect
 	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
--- main.go
+++ main.go
@@ -1,32 +1,54 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
-	"github.com/gin-contrib/timeout"
+	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"main.go/logger"
 )
 
-func testResponse(c *gin.Context) {
-	c.String(http.StatusRequestTimeout, "timeout")
-}
-
-func slowHandler(c *gin.Context) {
-	time.Sleep(800 * time.Millisecond)
-	c.Status(http.StatusOK)
-}
-
 func main() {
-	r := gin.New()
+	router := gin.Default()
 
-	r.GET("/slow", timeout.New(
-		timeout.WithTimeout(500*time.Millisecond),
-		timeout.WithResponse(testResponse),
-	), slowHandler)
+	router.Use(ginzap.Ginzap(logger.ZapLogger, time.RFC3339, true))
 
-	if err := r.Run(":8080"); err != nil {
-		log.Fatal(err)
+	router.Use(ginzap.RecoveryWithZap(logger.ZapLogger, true))
+
+	router.GET("/ping", func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/panic", func(c *gin.Context){
+		panic("An unexpected error occurred!")
+	})
+
+	srv := &http.Server{
+		Addr: ":8080",
+		Handler: router,
 	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		logger.ZapLogger.Fatal("Failed to start server", zap.Error(err))
+	}
+
+	go func() {
+		logger.Info("start server")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatal(err.Error())
+		}
+	}()
+	
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	defer logger.Sync()
+	logger.Info("stop")
 }
